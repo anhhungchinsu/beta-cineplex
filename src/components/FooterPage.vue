2f88e9a96c24bf3d2115b90b5ce115e4e0b8ec02
anhhungchinsu <hanthuong847742@gmail.com>
2021-02-25 23:28:53 +0700

    save

---
+++ src/components/FooterPage.vue
@@ -0,0 +1,71 @@
+<template>
+    <div class="footer">
+      <div class="container">
+          <div class="row">
+              <div class="col-3">
+                  <a href="#"><img height="55" src="../assets/img/logo.png"></a>
+                  <ul>
+                    <li><a href="#">Tuyển dụng</a></li>
+                    <li><a href="#">Giới thiệu</a></li>
+                    <li><a href="#">Liên hệ</a></li>
+                    <li><a href="#">F.A.Q</a></li>
+                    <li><a href="#">Hoạt động xã hội</a></li>
+                    <li><a href="#">Điều khoản sử dụng</a></li>
+                    <li><a href="#">Liên hệ quảng cáo</a></li>
+                    <li><a href="#">Điều khoản bảo mật</a></li>
+                  </ul>
+              </div>
+              <div class="col-3">
+                <h4 class="font-weight-bold">CỤM RẠP BETA</h4>
+                <ul>
+                    <li><a href="#"> Beta Cinemas Quang Trung, tp Hồ Chí Minh - Khai trương 22/01/2021 - Hotline 0708 268 992</a></li>
+                    <li><a href="#"> Beta Cinemas Giải Phóng - Hotline 0585 680 360</a></li>
+                    <li><a href="#"> Beta Cinemas Thái Nguyên: Hotline 0867 460 053</a></li>
+                    <li><a href="#"> Beta Cinemas Thanh Xuân: Hotline 0869 133 540</a></li>
+                    <li><a href="#"> Beta Cinemas Mỹ Đình: Hotline 0866 154 610</a></li>
+                    <li><a href="#"> Beta Cinemas Thanh Hóa: Hotline 0325 360 249</a></li>
+                    <li><a href="#"> Beta Cinemas Bắc Giang: Hotline 0866 493 413</a></li>
+                    <li><a href="#"> Beta Cinemas Đan Phượng: Hotline 0347 470 628</a></li>
+                    <li><a href="#"> Beta Cinemas Long Xuyên: Hotline 0923 262 767</a></li>
+                    <li><a href="#"> Beta Cinemas Nha Trang: Hotline 0923 228 258</a></li>
+                    <li><a href="#"> Beta Cinemas Biên Hòa: Hotline 0347 517 650</a></li>
+                    <li><a href="#"> Meci Cinema Long Khánh: Hotline 0251 6556677</a></li>
+                    <li><a href="#"> Meci Cinema Long Thành: Hotline 0251 6556677</a></li>
+                    <li><a href="#"> Beta Cinex Phú Mỹ - Hotline 0886006310</a></li>
+                    <li><a href="#"> Tải Ứng Dụng Beta Cineplex</a></li>
+                </ul>
+              </div>
+              <div class="col-3">
+                <h4 class="font-weight-bold">KẾT NỐI VỚI CHÚNG TÔI</h4>
+                <img class="img-fluid" src="../assets/img/dathongbao.png" alt="">
+              </div>
+              <div class="col-3">
+                <h4 class="font-weight-bold">LIÊN HỆ</h4>
+                <h5>CÔNG TY CỔ PHẦN BETA MEDIA</h5>
+                <p>Giấy chứng nhận ĐKKD số: 0106633482</p>
+                <p>Tầng 3, số 595, đường Giải Phóng, phường Giáp Bát, quận Hoàng Mai, thành phố Hà Nội</p>
+                <h5>Liên hệ hợp tác kinh doanh:</h5>
+                <p>minh@betacorp.vn</p>
+              </div>
+          </div>
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "FooterPage"
+}
+</script>
+
+<style lang="scss" scoped>
+  .footer {
+    border-top: 2px solid #ccc;
+    padding-top: 1.5rem;
+  }
+  a {
+    color: #000;
+    font-weight: bold;
+    font-size: 12px;
+  }
+</style>
